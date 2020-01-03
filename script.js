7d0ea0739907eaf24fbc0e8416ac95540121fc45
tmilne1 <tmilne1@gmail.com>
2020-01-02 21:13:46 -0500

    selected links, added event listeners

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const triggers = document.querySelectorAll('.cool > li');
+const dropdownBackground = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function enter(){
+        console.log("enter")
+        console.log(this)
+        
+}
+
+function exit(){
+    console.log("exit")
+    console.log(this)
+
+}
+
+triggers.forEach((link) =>{
+    link.addEventListener('mouseover', enter)
+    link.addEventListener('mouseleave', exit)
+});
