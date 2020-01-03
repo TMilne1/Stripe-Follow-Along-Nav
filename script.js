d0f88b7d1eb054ebee52ef502fc4a4872a63e736
tmilne1 <tmilne1@gmail.com>
2020-01-02 22:23:25 -0500

    list of links appears on mouseover of main links

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const dropdownBackground = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function enter(){
-        console.log("enter")
-        console.log(this)
-        
+    console.log("enter")
+    console.log(this)
+    this.classList.add('trigger-enter')
+    setInterval(()=>{
+        this.classList.add('trigger-enter-active')
+    },200)
+    
+    
 }
 
 function exit(){
     console.log("exit")
     console.log(this)
+    this.classList.remove('trigger-enter')
+    this.classList.remove ('trigger-enter-active')
+    
 
 }
 
